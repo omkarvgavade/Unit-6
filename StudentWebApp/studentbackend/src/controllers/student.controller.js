c00788cdee3a9bb95bc770a4cac10e98763411fd
Omkar Gavade <omkarvgawade@gmail.com>
2021-10-07 21:22:06 +0530

    done with backend

--- StudentWebApp/studentbackend/src/controllers/student.controller.js
+++ StudentWebApp/studentbackend/src/controllers/student.controller.js
@@ -15,23 +15,25 @@ router.post("", async (req, res) => {
 
 // 2.get all users
 router.get("", async (req, res) => {
+    console.log(req.query)
     const page = + req.query.page || 1;
     const size = +req.query.size || 10;
-    console.log(req.query)
+
     const age = +req.query.age || false
     const gender = req.query.gender || false
     const city = req.query.city || false
-
+    const sort = + req.query.sort || false
     const offset = (page - 1) * size;
-    console.log(age, gender)
-    if (age || gender || city) {
-        const students2 = await Student.find({ $and: [{ "age": age ? age : { $ne: -1 } }, { "gender": gender ? gender : { $ne: gender } }, { "city": city ? city : { $ne: city } }] }).skip(offset).limit(size).lean().exec()
+    console.log(age, gender, sort)
+    if (age || gender || city || sort) {
+        const students2 = await Student.find({ $and: [{ "age": age ? age : { $ne: -1 } }, { "gender": gender ? gender : { $ne: gender } }, { "city": city ? city : { $ne: city } }] }).sort({ age: sort }).skip(offset).limit(size).lean().exec()
+
         const totalPages = Math.ceil(await Student.find({ $and: [{ "age": { $gt: age } }, { "gender": gender }, { "city": city }] }).countDocuments().lean().exec() / size)
 
         return res.status(200).json({ data: students2, pages: totalPages })
     } else {
         const students = await Student.find().skip(offset).limit(size).lean().exec()
-        const totalPages = Math.ceil(await Student.find().countDocuments().lean().exec() / size)
+        const totalPages = Math.ceil(await Student.find().sort({ age: sort }).countDocuments().lean().exec() / size)
         return res.status(200).json({ data: students, pages: totalPages })
     }
 
@@ -59,5 +61,14 @@ router.delete("/:id", async (req, res) => {
     return res.status(200).json({ student })
 })
 
+router.get("/age/desc", async (req, res) => {
+    const students = await Student.find().sort({ age: -1 }).lean().exec()
+    return res.status(200).json({ students })
+})
+
+router.get("/age/asce", async (req, res) => {
+    const students = await Student.find().sort({ age: 1 }).lean().exec()
+    return res.status(200).json({ students })
+})
 
 module.exports = router;
